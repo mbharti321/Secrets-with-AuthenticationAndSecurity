67ff0a540b345976eda2a83c0cf094ab632d572a
mbharti321 <mbharti321@gmail.com>
2021-10-27 23:28:29 +0530

    add initial files

---
+++ app.js
@@ -0,0 +1,23 @@
+//jshint esversion:6
+ 
+const express = require("express");
+const bodyParser = require("body-parser");
+ 
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static("public"))
+app.set("view engine", "ejs")
+ 
+
+
+
+
+
+
+app.get("/", function(req, res){
+  res.send("Hello");
+});
+ 
+app.listen(3000, function () {
+    console.log("Server Started!!\nListening to port: http://localhost:3000/")
+});
