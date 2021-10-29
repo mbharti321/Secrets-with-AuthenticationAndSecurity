c9a84aa879638f7ef11a2cd67988144d0b731ca5
mbharti321 <mbharti321@gmail.com>
2021-10-30 01:56:47 +0530

    add submit secret and show all users secrets dynamically

--- app.js
+++ app.js
@@ -35,7 +35,8 @@ mongoose.connect(dbConnectionString, { useNewUrlParser: true })
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -90,12 +91,50 @@ app.get("/register", function (req, res) {
     res.render("register");
 });
 app.get("/secrets", function (req, res) {
+    // if (req.isAuthenticated()) {
+    //     res.render("secrets");
+    // } else {
+    //     res.redirect("/login");
+    // }
+
+    User.find({"secret": {$ne: null}}, function(err, foundUsers){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUsers){
+               res.render("secrets", {usersWithSecrets: foundUsers})
+            }
+        }
+    })
+});
+
+
+app.get("/submit", function (req, res) {
     if (req.isAuthenticated()) {
-        res.render("secrets");
+        res.render("submit");
     } else {
         res.redirect("/login");
     }
 });
+
+app.post("/submit", function (req, res){
+    const submittedSecret = req.body.secret;
+    // console.log(req.user);
+    User.findById(req.user.id, function(err, foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser){
+                foundUser.secret = submittedSecret;
+                foundUser.save(function(){
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    })
+
+});
+
 app.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/");
